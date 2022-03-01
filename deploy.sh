5c806c8fb452b0b2cef2766c70737be8d9cd26d4
Prabhat Sharma <hi.prabhat@gmail.com>
2022-03-01 03:14:03 -0800

    analytics

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@
 aws s3 sync ./ s3://zincsearch-website/ --profile=zinc-prod
 
 # invalidate cloudfront cache so that latest files can be served
-aws cloudfront create-invalidation --distribution-id E6XV11ELRG0JN --paths=/* --profile=zinc-prod
+aws cloudfront create-invalidation --distribution-id E1RESO2ZLOD5I0 --paths=/* --profile=zinc-prod
